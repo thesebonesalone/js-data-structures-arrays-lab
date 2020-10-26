fdb56aca0636c340e740ce1ff43cd4a4d11c4171
thesebonesalone <duikhead2@gmail.com>
2020-10-26 13:19:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+
+let drivers = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+}
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+}
+function appendDriver(name) {
+    return drivers.concat(name);
+}
+function prependDriver(name) {
+    return [name].concat(drivers);
+}
+function removeLastDriver() {
+    return drivers.slice(0,drivers.length - 1);
+}
+function removeFirstDriver() {
+    return drivers.slice(1,drivers.length);
+}
